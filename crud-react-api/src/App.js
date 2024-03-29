f2237d6821f49c022cdc971ed9314e273c4f9aeb
Wisit018 <lipton_pink@hotmail.co.th>
2024-03-29 17:18:14 +0700

    Update 1

---
+++ crud-react-api/src/App.js
@@ -0,0 +1,14 @@
+
+import Navbar from './Navbar';
+import News from './news';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <News />
+    </div>
+  );
+}
+
+export default App;
